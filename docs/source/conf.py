237a0d3e67b5efdaf8b2d205a4943aeaca130db5
Anthony DeGennaro <adegennaro@bnl.gov>
2019-01-02 13:58:28 -0500

    Change conf.py file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,8 +14,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('/home/adegennaro/Projects/sphinxtest/'))
-sys.path.insert(0, os.path.abspath('/home/adegennaro/Projects/sphinxtest/src'))
+sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../src'))
 
 
 
